96609019ac76e0f70b2fe77b713c6fa573ba23f3
Kelly Keating <keating.kel@gmail.com>
2019-07-03 10:21:24 +1200

    add router to app

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Router, Link } from "@reach/router"
+
+let Dash = () => <div>React development has begun!</div>
+let Home = () => (
+  <div>
+    <h1>Home</h1>
+    <nav>
+      <Link to="/">Home</Link> |{" "}
+      <Link to="dashboard">Dashboard</Link>
+    </nav>
+  </div>
+)
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <Router>
+      <Home path="/" />
+      <Dash path="dashboard" />
+    </Router>
   )
 }
 
