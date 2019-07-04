f16dcca9c6aef70814128a1a58cdc03288ef02f0
Kelly Keating <keating.kel@gmail.com>
2019-07-05 01:27:12 +1200

    display blocks

--- client/components/SetsHome.jsx
+++ client/components/SetsHome.jsx
@@ -8,15 +8,34 @@ class Sets extends React.Component {
     constructor (props) {
         super(props)
         this.state = {
-            sets: []
+            core: [],
+            blocks: []
         }
     }
 
     componentDidMount () {
         getAllSets()
             .then(sets => {
-                this.setState({sets})
+                const blocks = sets.reduce((tally, set) => {
+                    let name = set.block || "core"
+                    tally[name] ? tally[name].push(set) : tally[name] = [set]
+                    return tally
+                  }, {})
+                const core = blocks.core
+                delete blocks.core
+
+                this.setState({core, blocks})
             })
+    }
+
+    renderBlock (block) {
+        let [name, sets] = block
+        return (
+            <>
+            <h4>{name}</h4>
+            {sets.map(set => <p><Link to={set.name} >{set.name}</Link></p>)}
+            </>
+        )
     }
 
     render () {
@@ -29,7 +48,8 @@ class Sets extends React.Component {
             </section>
             
             <section>
-                {this.state.sets.map(set => <p><Link to={set.name} >{set.name}</Link></p>)}    
+                {this.state.core.length && this.renderBlock(["Core", this.state.core])}
+                {Object.entries(this.state.blocks).map(block => this.renderBlock(block))}    
             </section>
     
             <Link to="/">Home</Link>
