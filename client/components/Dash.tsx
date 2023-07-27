0a1698e014fcfb18845799a33b3412c4a930e9c5
Kelly Keating <keating.kel@gmail.com>
2023-07-28 01:10:04 +1200

    add user login logout

--- client/components/Dash.tsx
+++ client/components/Dash.tsx
@@ -1,6 +1,9 @@
+import { useAuth0 } from '@auth0/auth0-react'
 import { Link, Outlet } from 'react-router-dom'
 
 function Dash() {
+  const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0()
+
   return (
     <>
       <h1>Dash</h1>
@@ -17,6 +20,17 @@ function Dash() {
         <Link to="/cards">Search</Link> for a specific card or view your{' '}
         <Link to="/collection">collection</Link>
       </div>
+
+      {(isAuthenticated && user) ? (
+        <>
+          <button onClick={() => logout()}>Logout</button>
+          <img src={user.picture} alt={user.nickname + ' icon'} />
+          <p>{user.nickname}</p>
+        </>
+      ) : (
+        <button onClick={() => loginWithRedirect()}>Login</button>
+      )}
+
       <Outlet />
     </>
   )
--- client/index.tsx
+++ client/index.tsx
@@ -1,10 +1,19 @@
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 import routes from './routes'
 
+// TODO: replace deprecated redirectUri
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
-    <RouterProvider router={routes} />,
+    <Auth0Provider
+      domain="dev-kelly.au.auth0.com"
+      clientId="f5HswUt5Q5lpGBLzgPQRtK6LLD8rpSvx"
+      redirectUri={window.location.origin}
+      audience="https://magic-awaaaay/api"
+    >
+      <RouterProvider router={routes} />
+    </Auth0Provider>,
   )
 })
