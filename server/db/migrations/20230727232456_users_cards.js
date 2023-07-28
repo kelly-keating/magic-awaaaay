407469621000e55f0164f364e21a23b00f5c2fac
Kelly Keating <keating.kel@gmail.com>
2023-07-29 00:15:44 +1200

    add users_cards table

---
+++ server/db/migrations/20230727232456_users_cards.js
@@ -0,0 +1,12 @@
+exports.up = (knex) => {
+  return knex.schema.createTable('users_cards', (table) => {
+    table.increments('id').primary()
+    table.string('user_id')
+    table.string('card_id').references('cards.id')
+    table.integer('quantity')
+  }) 
+}
+
+exports.down = (knex) => {
+  return knex.schema.dropTable('users_cards')
+}
