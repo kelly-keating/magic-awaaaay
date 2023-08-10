8b2a4ac568b971ed2252839c00e2562935ca9a1a
Kelly Keating <keating.kel@gmail.com>
2023-08-10 13:36:04 +1200

    display collection value

--- client/components/Collection.tsx
+++ client/components/Collection.tsx
@@ -6,12 +6,40 @@ import { Heading } from '@chakra-ui/react'
 import { Link } from './utils'
 import CardGrid from './CardGrid/Grid'
 
-import { getAllUserCards } from '../api'
+import { getAllUserCards, getCurrencies } from '../api'
 
 function Collection() {
   const { getAccessTokenSilently } = useAuth0()
   const [cards, setCards] = useState([] as Card[])
   const [cardCounts, setCardCounts] = useState(null as null | CardCounts)
+  const [totalValue, setTotalValue] = useState(0)
+
+  // TODO: tidy this thing
+  useEffect(() => {
+    if (cardCounts && cards.length) {
+      getCurrencies()
+        .then((currencies) => {
+          const getAvgNzd = (usd: string | null, eur: string | null) => {
+            if (usd && eur) {
+              return ((Number(usd) * currencies.usd) + (Number(eur) * currencies.eur)) / 2
+            } else if (usd) {
+              return Number(usd) * currencies.usd
+            } else if (eur) {
+              return Number(eur) * currencies.eur
+            } else {
+              return 0
+            }
+          }
+
+          return cards.reduce((total, card) => {
+            const { usd, usd_foil, eur, eur_foil } = card.prices
+            return getAvgNzd(usd, eur) + getAvgNzd(usd_foil, eur_foil) + total
+          }, 0)
+        })
+        .then((total) => setTotalValue(total))
+        .catch((err) => console.log('oopsy', err.message))
+    }
+  }, [cards, cardCounts])
 
   useEffect(() => {
     getAccessTokenSilently()
@@ -37,6 +65,7 @@ function Collection() {
       <section>
         <Heading as="h2">My Collection</Heading>
         <p>Look at all your pretty cards</p>
+        <p>Total value: {totalValue.toFixed(2)}</p>
       </section>
 
       <section>
