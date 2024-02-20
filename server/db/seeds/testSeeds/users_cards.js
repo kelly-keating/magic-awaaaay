cd4a093526e26d9beeb96c13a4df2c07c3a634f2
Kelly Keating <keating.kel@gmail.com>
2024-02-20 17:18:52 +1300

    simplify test user names

--- server/db/seeds/testSeeds/users_cards.js
+++ server/db/seeds/testSeeds/users_cards.js
@@ -7,28 +7,28 @@ exports.seed = async function (knex) {
   await knex('users_cards').insert([
     {
       id: 4,
-      user_id: 'auth0|nonExistentUser',
+      user_id: 'auth0|singleCardUser',
       card_id: '21c950d7-b4f6-4902-8c9a-98f2933f9fa5',
       quantity: 1,
       foil_quantity: 0,
     },
     {
       id: 5,
-      user_id: 'auth0|testUser',
+      user_id: 'auth0|multiCardUser',
       card_id: '9dc4d69c-f61d-4122-9e0b-c88aa905d159',
       quantity: 1,
       foil_quantity: 0,
     },
     {
       id: 7,
-      user_id: 'auth0|testUser',
+      user_id: 'auth0|multiCardUser',
       card_id: '8c39f9b4-02b9-4d44-b8d6-4fd02ebbb0c5',
       quantity: 4,
       foil_quantity: 1,
     },
     {
       id: 6,
-      user_id: 'auth0|testUser',
+      user_id: 'auth0|multiCardUser',
       card_id: '65eb6cda-e512-40a8-9c1f-335b713409ff',
       quantity: 0,
       foil_quantity: 10,
--- server/db/tests/userCards.test.ts
+++ server/db/tests/userCards.test.ts
@@ -13,7 +13,7 @@ import {
 
 describe('checkUserCardExists', () => {
   it('returns TRUE if card is associated with that user', async () => {
-    const user = 'auth0|testUser'
+    const user = 'auth0|multiCardUser'
     const card = '65eb6cda-e512-40a8-9c1f-335b713409ff'
 
     const cardExists = await checkUserCardExists(card, user)
@@ -21,7 +21,7 @@ describe('checkUserCardExists', () => {
   })
 
   it('returns FALSE if card is not yet owned by user', async () => {
-    const user = 'auth0|testUser'
+    const user = 'auth0|multiCardUser'
     const card = 'card-that-does-not-exist-in-db'
 
     const cardExists = await checkUserCardExists(card, user)
@@ -31,13 +31,13 @@ describe('checkUserCardExists', () => {
 
 describe('getUsersCards', () => {
   it('returns empty object when user has no cards', async () => {
-    const user = 'auth0|notAUser'
+    const user = 'notAUser'
     const actual = await getUsersCards(user)
     expect(actual).toEqual({})
   })
 
   it('returns all users cards (ONE CARD)', async () => {
-    const user = 'auth0|nonExistentUser'
+    const user = 'auth0|singleCardUser'
 
     const cards = {
       '21c950d7-b4f6-4902-8c9a-98f2933f9fa5': { foil: 0, normal: 1 },
@@ -47,7 +47,7 @@ describe('getUsersCards', () => {
   })
 
   it('returns all users cards (MANY CARDS)', async () => {
-    const user = 'auth0|testUser'
+    const user = 'auth0|multiCardUser'
 
     const cards = {
       '65eb6cda-e512-40a8-9c1f-335b713409ff': { foil: 10, normal: 0 },
@@ -61,7 +61,7 @@ describe('getUsersCards', () => {
 
 describe('getUsersCardsFromSet', () => {
   it('successfully accesses user cards by set name', async () => {
-    const user = 'auth0|testUser'
+    const user = 'auth0|multiCardUser'
     // BUG: should this be set code rather than set name?
     const set = 'Magic 2010'
     const cards = {
@@ -73,7 +73,7 @@ describe('getUsersCardsFromSet', () => {
   })
 
   it('returns an empty object when no user cards are from that set', async () => {
-    const user = 'auth0|testUser'
+    const user = 'auth0|multiCardUser'
     const set = 'Magic 2099'
     const cards = {}
 
@@ -91,7 +91,7 @@ describe('getCardsByUserId', () => {
   })
 
   it('returns empty object for user with no cards', async () => {
-    const user = 'auth0|testUser'
+    const user = 'auth0|multiCardUser'
     const cards = await getCardsByUserId(user)
 
     expect(cards).toHaveLength(3)
@@ -117,7 +117,7 @@ describe('getAllCardInfoForUser', () => {
   })
 
   it('returns all details for existing user', async () => {
-    const user = 'auth0|testUser'
+    const user = 'auth0|multiCardUser'
     const { userCards, cards } = await getAllCardInfoForUser(user)
 
     expect(userCards).toEqual({
@@ -138,7 +138,7 @@ describe('getAllCardInfoForUser', () => {
 describe('addCardToUser', () => {
   it("returns the user's card with all details", async () => {
     const newCard = {
-      user_id: 'testUser',
+      user_id: 'notAUser',
       card_id: '1234567890',
       foil_quantity: 1,
       quantity: 4,
@@ -151,21 +151,24 @@ describe('addCardToUser', () => {
 
   it('actually adds the record to the database', async () => {
     const newCard = {
-      user_id: 'testUser2',
+      user_id: 'auth0|singleCardUser',
       card_id: '0987654321',
       foil_quantity: 0,
       quantity: 1,
     }
     const fullUserCard = { ...newCard, id: 9 }
 
-    const startCards = await getUsersCards('testUser2')
-    expect(startCards).toEqual({})
+    const startCards = await getUsersCards('auth0|singleCardUser')
+    expect(startCards).toEqual({
+      '21c950d7-b4f6-4902-8c9a-98f2933f9fa5': { foil: 0, normal: 1 },
+    })
 
     const actual = await addCardToUser(newCard)
     expect(actual).toEqual(fullUserCard)
 
-    const endCards = await getUsersCards('testUser2')
+    const endCards = await getUsersCards('auth0|singleCardUser')
     expect(endCards).toEqual({
+      '21c950d7-b4f6-4902-8c9a-98f2933f9fa5': { foil: 0, normal: 1 },
       '0987654321': { normal: 1, foil: 0 },
     })
   })
@@ -174,7 +177,7 @@ describe('addCardToUser', () => {
 describe('updateUserCard', () => {
   it('returns the new card information', async () => {
     const newDetails = {
-      user_id: 'auth0|nonExistentUser',
+      user_id: 'auth0|singleCardUser',
       card_id: '21c950d7-b4f6-4902-8c9a-98f2933f9fa5',
       quantity: 100,
       foil_quantity: -1,
@@ -187,16 +190,16 @@ describe('updateUserCard', () => {
 
   it('successfully updates the user card', async () => {
     const newDetails = {
-      user_id: 'auth0|testUser',
+      user_id: 'auth0|multiCardUser',
       card_id: '9dc4d69c-f61d-4122-9e0b-c88aa905d159',
       quantity: 2,
       foil_quantity: 0,
     }
     const expectedResult = { ...newDetails, id: 5 }
 
-    const startCards = await getUsersCards('auth0|testUser')
+    const startCards = await getUsersCards('auth0|multiCardUser')
     const returnedCard = await updateUserCard(newDetails)
-    const endCards = await getUsersCards('auth0|testUser')
+    const endCards = await getUsersCards('auth0|multiCardUser')
 
     expect(returnedCard).toEqual(expectedResult)
     expect(endCards).not.toEqual(startCards)
